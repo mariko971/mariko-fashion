af6b26360cc30934f7ab1eed7556888cb6fb4660
Mariko971 <slimmack@gmail.com>
2021-07-16 15:52:09 +0400

    added redux-saga to our store and created root.sagas file for handling general saga operations

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,16 +1,24 @@
 import { createStore, applyMiddleware } from 'redux';
 import logger from 'redux-logger';
 import { persistStore } from 'redux-persist';
-import thunk from 'redux-thunk';
+import createSagaMiddleware from 'redux-saga';
+import { composeWithDevTools } from 'redux-devtools-extension';
+
 
 import rootReducer from './root-reducer';
+import { rootSaga } from './root.sagas';
 
-const middleWares = [ thunk];
+const sagaMiddleware = createSagaMiddleware();
+
+const middleWares = [ sagaMiddleware];
 if(process.env.NODE_ENV==='development'){
     middleWares.push(logger);
 }
 
-export const store = createStore(rootReducer, applyMiddleware(...middleWares));
+
+export const store = createStore(rootReducer, composeWithDevTools(applyMiddleware(...middleWares)));
+sagaMiddleware.run(rootSaga);
 export const persistor = persistStore(store);
+
 
 
